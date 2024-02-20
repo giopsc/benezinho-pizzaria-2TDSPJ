67bff4fd13b75da42bf82c8e31ed021606ac5414
Benefrancis <benefrancis@gmail.com>
2024-02-20 11:57:56 -0300

    mostrando lombok

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,5 +1,6 @@
 package br.com.fiap;
 
+import br.com.fiap.domain.entity.Opcional;
 import br.com.fiap.domain.entity.Pizzaria;
 import br.com.fiap.domain.entity.Produto;
 import br.com.fiap.domain.entity.Sabor;
@@ -14,13 +15,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory( "maria-db" );
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("fiap");
         EntityManager manager = factory.createEntityManager();
 
-        Pizzaria pizzaria = manager.find( Pizzaria.class, 1 );
+        salvar(manager);
+
+        Opcional borda = new Opcional(null, "Borda de Catupiri", 19.99);
+
+        Opcional cocaCola = Opcional.builder().id(null).nome("Coca Cola").preco(19.99).build();
+
+        manager.getTransaction().begin();
+        manager.persist(borda);
+        manager.persist(cocaCola);
+        manager.getTransaction().commit();
 
 
-        JOptionPane.showMessageDialog( null, pizzaria );
+        Pizzaria pizzaria = manager.find(Pizzaria.class, 1);
+
+        JOptionPane.showMessageDialog(null, pizzaria);
 
         manager.close();
         factory.close();
@@ -29,20 +41,20 @@ public class Main {
 
     private static void salvar(EntityManager manager) {
         Pizzaria dominus = new Pizzaria();
-        dominus.setId( null ).setNome( "Dominus" );
+        dominus.setId(null).setNome("Dominus");
 
-        Sabor frangoComCatupiri = new Sabor( null, "Frango com Catupiri", "Deliciosa pizza de frango com o autentico Catupiri" );
+        Sabor frangoComCatupiri = new Sabor(null, "Frango com Catupiri", "Deliciosa pizza de frango com o autentico Catupiri");
 
-        Sabor caipira = new Sabor( null, "Caipira", "Deliciosa caipira com milho macio" );
+        Sabor caipira = new Sabor(null, "Caipira", "Deliciosa caipira com milho macio");
 
-        var pizzaDeFrangoCatu = new Produto( null, "Pizza", BigDecimal.valueOf( 59.98 ), frangoComCatupiri );
+        var pizzaDeFrangoCatu = new Produto(null, "Pizza", BigDecimal.valueOf(59.98), frangoComCatupiri);
 
 
         manager.getTransaction().begin();
-        manager.persist( frangoComCatupiri );
-        manager.persist( caipira );   //sql INSERT INTO
-        manager.persist( dominus );
-        manager.persist( pizzaDeFrangoCatu );
+        manager.persist(frangoComCatupiri);
+        manager.persist(caipira);   //sql INSERT INTO
+        manager.persist(dominus);
+        manager.persist(pizzaDeFrangoCatu);
         manager.getTransaction().commit();
     }
 }
---
+++ src/main/java/br/com/fiap/domain/entity/Opcional.java
@@ -0,0 +1,30 @@
+package br.com.fiap.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "TB_OPCIONAL")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Opcional {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_OPCIONAL")
+    @SequenceGenerator(
+            name = "SQ_OPCIONAL", sequenceName = "SQ_OPCIONAL"
+    )
+    @Column(name = "ID_OPCIONAL")
+    private Long id;
+
+    @Column(name = "NM_OPCIONAL")
+    private String nome;
+
+    @Column(name = "PRECO")
+    private Double preco;
+
+}
